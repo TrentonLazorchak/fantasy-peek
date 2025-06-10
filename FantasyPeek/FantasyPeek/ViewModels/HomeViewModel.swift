cc4f2df49da6239a8832cd7cb5b5530219f1d983
Trenton Lazorchak <>
2025-06-09 20:50:59 -0400

    Write view model for league info.

--- FantasyPeek/FantasyPeek/ViewModels/HomeViewModel.swift
+++ FantasyPeek/FantasyPeek/ViewModels/HomeViewModel.swift
@@ -7,7 +7,47 @@
 
 import Observation
 
+struct LeagueInfoViewModel {
+    let id: String
+    let name: String
+    let totalRosters: Int
+
+    init(sleeperLeagueInfo: SleeperLeagueInfoModel) {
+        id = sleeperLeagueInfo.leagueId
+        name = sleeperLeagueInfo.name
+        totalRosters = sleeperLeagueInfo.totalRosters
+    }
+}
+
 @Observable @MainActor
 final class HomeViewModel {
+
+    let manager: SleeperManaging
+    init(manager: SleeperManaging = SleeperManager()) {
+        self.manager = manager
+    }
+
+    let leagueID: String = ""
+    var leagueInfo: LeagueInfoViewModel?
+
+    var viewState: ViewState = .loaded
+    enum ViewState {
+        case loading
+        case loaded
+        case failure
+    }
+
+    func fetchSleeperLeagueInfo() async {
+        viewState = .loading
+
+        do {
+            let sleeperLeagueInfo = try await manager.fetchLeagueInfo(leagueID: leagueID)
+            leagueInfo = .init(sleeperLeagueInfo: sleeperLeagueInfo)
+            viewState = .loaded
+            // TODO: Navigate to tab bar of pages as listed in notes
+        } catch {
+            viewState = .failure
+        }
+    }
 
 }

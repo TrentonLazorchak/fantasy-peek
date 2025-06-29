fcffb28a327c2e7592ffcf343124a92258397969
Trenton Lazorchak <>
2025-06-29 10:30:43 -0400

    Update manager files to include cache option.

--- FantasyPeek/FantasyPeek/Managers/SleeperManager.swift
+++ FantasyPeek/FantasyPeek/Managers/SleeperManager.swift
@@ -8,44 +8,78 @@
 import Foundation
 
 protocol SleeperManaging {
-    func fetchAllLeagues(for username: String, sport: String, season: String) async throws -> [SleeperLeagueInfoModel]?
+    func fetchAllLeagues(username: String, sport: String, season: String) async throws -> [SleeperLeagueInfoModel]?
     func fetchLeagueInfo(leagueID: String) async throws -> SleeperLeagueInfoModel
+    func fetchAllRosters(leagueID: String) async throws -> [SleeperRosterModel]
+    func fetchDisplayName(userID: String) async throws -> String
+    func fetchAllNFLPlayers() async throws -> [SleeperPlayersResponse]
 }
 
 extension SleeperManaging {
     // Provides default values for sport and season. Only "nfl" is available for sport at the moment
-    func fetchAllLeagues(for username: String, sport: String = "nfl", season: String) async throws -> [SleeperLeagueInfoModel]? {
-        try await fetchAllLeagues(for: username, sport: sport, season: season)
+    func fetchAllLeagues(username: String, sport: String = "nfl", season: String) async throws -> [SleeperLeagueInfoModel]? {
+        try await fetchAllLeagues(username: username, sport: sport, season: season)
     }
 }
 
 final class SleeperManager: SleeperManaging {
+
+    private static let baseURL = "https://api.sleeper.app/v1"
+
     // NOTE: Only support "nfl" for sport right now
-    func fetchAllLeagues(for username: String, sport: String, season: String) async throws -> [SleeperLeagueInfoModel]? {
+    func fetchAllLeagues(username: String, sport: String, season: String) async throws -> [SleeperLeagueInfoModel]? {
         // Call to get user ID from the passed in username
-        guard let userURL = URL(string: "https://api.sleeper.app/v1/user/\(username)") else {
-            assertionFailure("Invalid user URL")
-            throw URLError(.badURL)
-        }
-        let (userData, _) = try await URLSession.shared.data(from: userURL)
-        let userID = try JSONDecoder().decode(SleeperUser.self, from: userData).userID
+        let userID = try await fetchUser(user: username).userID
 
-        guard let url = URL(string: "https://api.sleeper.app/v1/user/\(userID)/leagues/\(sport)/\(season)") else {
-            assertionFailure("Invalid leagues URL")
-            throw URLError(.badURL)
-        }
-
-        let (data, _) = try await URLSession.shared.data(from: url)
-        return try JSONDecoder().decode([SleeperLeagueInfoModel]?.self, from: data)
+        return try await fetch(urlString: "\(Self.baseURL)/user/\(userID)/leagues/\(sport)/\(season)", as: [SleeperLeagueInfoModel]?.self)
     }
 
     func fetchLeagueInfo(leagueID: String) async throws -> SleeperLeagueInfoModel {
-        guard let url = URL(string: "https://api.sleeper.app/v1/league/\(leagueID)") else {
+        try await fetch(urlString: "\(Self.baseURL)/league/\(leagueID)", as: SleeperLeagueInfoModel.self)
+    }
+
+    func fetchAllRosters(leagueID: String) async throws -> [SleeperRosterModel] {
+        try await fetch(urlString: "\(Self.baseURL)/league/\(leagueID)/rosters", as: [SleeperRosterModel].self)
+    }
+
+    func fetchDisplayName(userID: String) async throws -> String {
+        try await fetchUser(user: userID).displayName
+    }
+
+    func fetchAllNFLPlayers() async throws -> [SleeperPlayersResponse] {
+        try await fetch(urlString: "\(Self.baseURL)/players/nfl", as: [SleeperPlayersResponse].self, useCache: true)
+    }
+
+    private func fetch<T: Codable>(urlString: String, as type: T.Type, useCache: Bool = false) async throws -> T {
+        // Check if cached response exists
+        if useCache,
+           let cachedData = try await CacheManager.getCachedData(forKey: urlString) {
+            let decoded = try JSONDecoder().decode(T.self, from: cachedData)
+            return decoded
+        }
+
+        guard let url = URL(string: urlString) else {
             assertionFailure("Invalid URL")
             throw URLError(.badURL)
         }
 
         let (data, _) = try await URLSession.shared.data(from: url)
-        return try JSONDecoder().decode(SleeperLeagueInfoModel.self, from: data)
+        let decoded = try JSONDecoder().decode(T.self, from: data)
+
+        // Cache the valid response
+        if useCache {
+            try await CacheManager.cacheData(decoded, forKey: urlString)
+        }
+
+        return decoded
+    }
+
+    private func fetchUser(user: String) async throws -> SleeperUserModel {
+        guard let userURL = URL(string: "\(Self.baseURL)/user/\(user)") else {
+            assertionFailure("Invalid user URL")
+            throw URLError(.badURL)
+        }
+        let (userData, _) = try await URLSession.shared.data(from: userURL)
+        return try JSONDecoder().decode(SleeperUserModel.self, from: userData)
     }
 }

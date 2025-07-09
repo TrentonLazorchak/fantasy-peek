c1f3e9602b0ce17894768ca21594d1bb8210f743
TEST Trenton <>
2025-07-09 14:25:20 -0400

    Team view model summary unit test

---
+++ FantasyPeek/FantasyPeekTests/ViewModels/TeamViewModelTests.swift
@@ -0,0 +1,43 @@
+//
+//  TeamViewModelTests.swift
+//  FantasyPeek
+//
+//  Created by Trenton Lazorchak on 7/9/25.
+//
+
+import Testing
+@testable import FantasyPeek
+
+/// Unit tests for the roster view model
+@MainActor
+@Suite("TeamViewModelTests")
+struct TeamViewModelTests {
+
+    @Test("Tests the team view model summary")
+    func testSummary() async {
+        let team = TeamViewModel(
+            userDisplayName: "UserDisplayName",
+            teamName: "TeamName",
+            avatar: "Avatar",
+            starters: [.init(
+                playerID: "PlayerID",
+                name: "Name",
+                position: "QB",
+                team: "WAS"
+            )],
+            bench: [.init(
+                playerID: "BenchPlayerID",
+                name: "BenchName",
+                position: "RB",
+                team: "BAL"
+            )],
+            wins: 0,
+            losses: 0,
+            ties: 0,
+            index: 0
+        )
+        
+        #expect(team.summary == "Team 1: TeamName\nManager: UserDisplayName\nRecord: 0-0-0\nStarters:\n• Name - QB (WAS)\nStarters:\n• Name - QB (WAS)\nBench:\n• BenchName - RB (BAL)")
+    }
+
+}
